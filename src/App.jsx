4021fc7d1e0cce309ac60cdf9548a3d0e580e733
vinay <123vinaymaurya@gmail.com>
2024-04-04 10:51:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<ToastContainer
+				hideProgressBar={true}
+				stacked
+			/>
+			<Container className='my-2'>
+				<Outlet />
+			</Container>
+		</>
+	);
+}
+
+export default App;
